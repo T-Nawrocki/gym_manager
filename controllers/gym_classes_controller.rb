893c992a90f4e4414e06d1ef0dd8a2ffb4b8a086
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-07-13 12:11:17 +0100

    minor cleaning up of page headers

--- controllers/gym_classes_controller.rb
+++ controllers/gym_classes_controller.rb
@@ -3,12 +3,23 @@ require "sinatra/contrib/all"
 require_relative "../models/gym_class"
 also_reload("../models/*")
 
+
 get "/class-list" do
     @gym_classes = GymClass.all
     erb(:"gym_classes/index")
 end
 
+
+get "/class/new" do
+    erb(:"gym/classes/new")
+end
+
+post "/class/new" do
+end
+
+
 get "/class/:id" do
     @gym_class = GymClass.find(params["id"])
     erb(:"gym_classes/show")
 end
+
--- controllers/members_controller.rb
+++ controllers/members_controller.rb
@@ -3,22 +3,25 @@ require "sinatra/contrib/all"
 require_relative "../models/member"
 also_reload("../models/*")
 
+
 get "/members-list" do
     @members = Member.all
     erb(:"members/index")
 end
 
+
 get "/member/new" do
     erb(:"members/new")
 end
 
-get "/member/:id" do
-    @member = Member.find(params["id"])
-    erb(:"members/show")
-end
-
-post "/members-list" do
+post "/member/new" do
     new_member = Member.new(params)
     new_member.save
     redirect "/members-list"
+end
+
+
+get "/member/:id" do
+    @member = Member.find(params["id"])
+    erb(:"members/show")
 end
