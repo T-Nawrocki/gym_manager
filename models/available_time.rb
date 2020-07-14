e0b5d863b3f7d1b69634653f97407097136df5a9
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-07-14 11:45:12 +0100

    available times basic crud methods written

--- models/available_time.rb
+++ models/available_time.rb
@@ -1,3 +1,5 @@
+require_relative "../db/sql_runner"
+
 class AvailableTime
     
     attr_reader :id, :gym_class_id, :day, :time
@@ -10,6 +12,36 @@ class AvailableTime
     end
 
 
-    # 
+    # === CRUD METHODS ===
+    # CREATE
+    def save
+        sql = "INSERT INTO available_times
+        (gym_class_id, day, time)
+        VALUES ($1, $2, $3)
+        RETURNING id"
+        values = [@gym_class_id, @day, @time]
+
+        result = SqlRunner.run(sql, values).first
+        @id = result["id"].to_i
+    end
+
+    # READ
+    # Omitted—available times will only be read as part of GymClass.times
+
+    # UPDATE
+    # Omitted—design decision to only allow creation/deletion
+
+    # DELETE
+    def self.delete_all
+        sql = "DELETE FROM available_times"
+        SqlRunner.run(sql)
+    end
+
+    def delete
+        sql = "DELETE FROM available_times
+        WHERE id = $1"
+        values = [@id]
+        SqlRunner.run(sql, values)
+    end
 
 end
